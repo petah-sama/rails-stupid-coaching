8f0cc6c3a78f0361c0abd7dd364e669d566ba0a5
Pedro Batista <pedrobatista.420@gmail.com>
2020-02-17 14:27:39 +0000

    done w/ test

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params['question']
+
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
